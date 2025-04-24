bb118dc653fd01e8203a1532981c836b131afe9a
lima48927 <lima48927@gmail.com>
2025-04-24 19:55:04 -0300

    Update botcopy.py

--- botcopy.py
+++ botcopy.py
@@ -2,16 +2,17 @@ from telethon.sync import TelegramClient, events
 import re
 
 # Dados do bot
-api_id = 2408062
-api_hash = '91af58f5c8faf3586729b242e76a66d67'
+api_id = 24080662
+api_hash = '91af588f5c8faf3586729b042e76a66d67'
 bot_token = '8049772298:AAEZtGExb1DLcxUkjeUmIRLx7pq49if1tTU'
 bot_username = 'lima48927'
 grupo_origem = -2691858749
 grupo_destino = -2126433918
 link_customizado = 'https://stke.me/l/436/761'
 
-# Conecta ao cliente do Telegram (usando o usuário do bot)
-client = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
+# Conecta ao cliente do Telegram (usando token do bot)
+client = TelegramClient('bot', api_id, api_hash)
+client.start(bot_token=bot_token)
 
 # Função para substituir todos os links
 def substituir_links(texto):
